4702e2d5754e0a47c490ad41eee1fdfb134c2933
Chris Ru <chris.ru@gmail.com>
2023-03-09 10:26:35 -0600

    Add tutorials for install aws instance and docker container

---
+++ terraform-training/lab_install_aws_instance/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0f3c9c466bb525749"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
